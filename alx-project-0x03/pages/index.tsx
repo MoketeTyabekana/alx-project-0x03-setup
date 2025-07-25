0eaa2830dda27aa83ddd0bc341bb5053305cc957
Mokete <ematyabekana@gmail.com>
2025-07-25 15:13:48 +0200

    Updated the home page to use the const router = useRouter(); for navigation.

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,12 +1,13 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 
+
 interface PageRouteProps {
   pageRoute: string
 }
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
